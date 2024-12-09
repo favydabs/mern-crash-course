153c9f5f3f6df9a87677936bf277ab5b92e09959
favydabs <favourdabit@gmail.com>
2024-12-09 10:04:22 +0100

    complete project

--- MERN-CRASH-COURSE/backend/server.js
+++ MERN-CRASH-COURSE/backend/server.js
@@ -1,15 +1,28 @@
 import express from "express";
-import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import productRoutes from "./routes/product.routes.js";
+import path from "path";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // allow us to accept json data
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
